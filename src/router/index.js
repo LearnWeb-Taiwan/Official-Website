ccd893b44506e8b164904b2ec1f2c47cc5368257
ShawnL <shawnlin0201@gmail.com>
2020-05-26 19:22:21 +0800

    init activities page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -8,13 +7,13 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: () => import('../views/Home.vue'),
   },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import('../views/About.vue')
-  // }
+  {
+    path: '/activities',
+    name: 'Activities',
+    component: () => import('../views/Activities.vue'),
+  },
 ]
 
 const router = new VueRouter({
