c2ec189b4579f55dc6319e41ab15775c8e2af7c7
ShawnL <shawnlin0201@gmail.com>
2020-05-26 19:23:10 +0800

    sample component

---
+++ src/components/sample.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="componentName-wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: 'sample',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@media screen and (max-width: 600px) {
+}
+</style>
