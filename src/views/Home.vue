0f7c3e8917cd68b171ace09d8577f840788bad65
ShawnL <shawnlin0201@gmail.com>
2020-05-28 14:48:11 +0800

    banner RWD done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,14 +2,15 @@
   <div class="home-wrapper">
     <section class="banner-wrapper">
       <div class="banner-slogan container">
-        <p class="banner-title">LearnWeb</p>
-        <p class="banner-subtitle">學習使你永不匱乏，熱情使你永不空虛。</p>
+        <p class="banner-title">It's time for you!</p>
+        <p class="banner-subtitle">學習使人永不匱乏，熱情使人永不空虛。</p>
         <p class="banner-content">
-          這是一個使<span class="highlight">入門</span>
+          這是一個使
+          <span class="highlight">初階</span>
           網頁開發者能輕鬆分享與互動的開源社群。<br />
-          不定時舉辦
+          我們不定時舉辦
           <span class="highlight">講座</span>、
-          <span class="highlight">工作坊</span>與
+          <span class="highlight">工作坊</span> 與
           <span class="highlight">讀書會</span>，<br />
           激發你對於網頁開發中的無限創意。<br /><br />
           準備好認識更多新朋友，一起享受美好的討論時光了嗎？
@@ -86,44 +87,133 @@ export default {
       }
     }
     .banner-btn {
+      cursor: pointer;
       display: inline-block;
       font-size: 24px;
       padding: 8px 16px;
-      background: #f0822b;
+      background: #4f74af;
       margin-top: 40px;
       position: relative;
+      top: 0;
+      left: 0;
       z-index: 2;
-      color: transparent;
+      transition: 0.3s;
+      &:hover {
+        &::after {
+          top: -12px;
+          left: 8px;
+          background: #000000;
+        }
+        &::before {
+          top: 8px;
+          left: 16px;
+        }
+      }
+      &::after {
+        content: '立即參與';
+        padding: 8px 16px;
+        box-sizing: border-box;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: 2;
+        width: 100%;
+        height: 100%;
+        background: #000000;
+        transition: 0.3s;
+      }
       &::before {
         content: '立即參與';
         padding: 8px 16px;
+        box-sizing: border-box;
         position: absolute;
-        top: -12px;
-        left: 24px;
-        background: #4f74af;
-        color: transparent;
-        z-index: 1;
-        white-space: nowrap;
-      }
-      &::after {
-        cursor: pointer;
-        content: '立即參與';
-        padding: 8px 16px;
-        position: absolute;
-        top: 12px;
-        left: 12px;
-        background: #00000090;
-        color: white;
+        top: 0;
+        left: 0;
         z-index: 2;
-        white-space: nowrap;
+        width: 100%;
+        height: 100%;
+        background: #f0822b;
+        transition: 0.3s;
+        color: transparent;
       }
     }
   }
 }
 
 @media screen and (max-width: 1200px) {
+  .banner-wrapper {
+    .banner-slogan {
+      .banner-title {
+        padding: 8px;
+        font-size: 24px;
+      }
+      .banner-subtitle {
+        padding: 8px;
+        font-size: 24px;
+        background: #00000090;
+      }
+      .banner-content {
+        margin-top: 24px;
+        padding: 8px;
+        font-size: 16px;
+      }
+      .banner-btn {
+        font-size: 14px;
+        margin-top: 24px;
+        &:hover {
+          &::after {
+            top: -8px;
+            left: 4px;
+          }
+          &::before {
+            top: 4px;
+            left: 12px;
+          }
+        }
+      }
+    }
+  }
 }
 
 @media screen and (max-width: 600px) {
+  .banner-wrapper {
+    .banner-slogan {
+      left: 50%;
+      transform: translateX(-50%);
+      text-align: center;
+      .banner-title {
+        display: none;
+      }
+      .banner-subtitle {
+        min-width: 180px;
+        max-width: 180px;
+        padding: 8px;
+        font-size: 20px;
+        background: #00000080;
+      }
+      .banner-content {
+        display: none;
+      }
+      .banner-btn {
+        font-size: 20px;
+        margin-top: 40px;
+        &::after {
+          content: '立即參與實體聚會';
+          width: 200px;
+          left: -50px;
+        }
+        &:hover {
+          &::after {
+            top: 0px;
+            left: -50px;
+          }
+          &::before {
+            top: 0px;
+            left: -50px;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
