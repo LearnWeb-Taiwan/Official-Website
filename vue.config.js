e0a63f4d5392f6fa26b69db40b432eee4fd2b707
ShawnL <shawnlin0201@gmail.com>
2020-06-01 18:58:01 +0800

    change public path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Official-Website' : '/',
   css: {
     loaderOptions: {
       scss: {
