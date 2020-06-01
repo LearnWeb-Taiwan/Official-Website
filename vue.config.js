383b2f3afe0225afbc0fc0b2d46dcbbf45131a94
Shawn Lin <45999699+shawnlin0201@users.noreply.github.com>
2020-06-02 00:55:47 +0800

    change root path

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Official-Website' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   css: {
     loaderOptions: {
       scss: {
