af29cfac31cca57bbad4e8ff769b8ae595cef7b8
ShawnL <shawnlin0201@gmail.com>
2020-06-08 15:37:30 +0800

    add slider controller btn click test

--- tests/unit/PicSlider.spec.js
+++ tests/unit/PicSlider.spec.js
@@ -2,7 +2,7 @@ import { mount } from '@vue/test-utils'
 import PicSlider from '@/components/PicSlider.vue'
 
 describe('PicSlider', () => {
-  it('prop imgSet prepare', () => {
+  it('prepare prop imgSet', () => {
     const imageSet = ['/mock1.png', '/mock2.png', '/mock3.png']
     const slideSec = 1
     const needAutoSlide = false
@@ -24,6 +24,37 @@ describe('PicSlider', () => {
 
     expect(wrapper.findAll('.slider-img>img').at(2).html()).toBe(
       `<img src="${imageSet[2]}" alt="slide-img" class="" style="transition: ${slideSec}s;">`
+    )
+  })
+
+  it('click slider controll btn', async () => {
+    const imageSet = ['/mock1.png', '/mock2.png', '/mock3.png']
+    const slideSec = 1
+    const needAutoSlide = false
+    const wrapper = mount(PicSlider, {
+      propsData: {
+        imageSet,
+        slideSec,
+        needAutoSlide,
+      },
+    })
+
+    expect(wrapper.findAll('.slider-img>img').at(0).html()).toBe(
+      `<img src="${imageSet[0]}" alt="slide-img" class="isActive" style="transition: ${slideSec}s;">`
+    )
+
+    wrapper.findAll('.slider-controller-circle').at(1).trigger('click')
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.findAll('.slider-img>img').at(1).html()).toBe(
+      `<img src="${imageSet[1]}" alt="slide-img" class="isActive" style="transition: ${slideSec}s;">`
+    )
+
+    wrapper.findAll('.slider-controller-circle').at(2).trigger('click')
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.findAll('.slider-img>img').at(2).html()).toBe(
+      `<img src="${imageSet[2]}" alt="slide-img" class="isActive" style="transition: ${slideSec}s;">`
     )
   })
 })
