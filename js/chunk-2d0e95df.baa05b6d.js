b827de56d4aa986ea5a49798366d62d6cbae65cd
ShawnL <45999699+shawnlin0201@users.noreply.github.com>
2023-05-01 14:07:31 +0800

    deploy

---
+++ js/chunk-2d0e95df.baa05b6d.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e95df"],{"8cdb":function(e,n,t){"use strict";t.r(n);var r=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"404-wrapper"},[t("h1",[e._v("This is an 404 page")])])}],a={name:"error",components:{}},c=a,i=t("2877"),l=Object(i["a"])(c,r,s,!1,null,null,null);n["default"]=l.exports}}]);
+//# sourceMappingURL=chunk-2d0e95df.baa05b6d.js.map
---
+++ js/chunk-2d209591.b6372e68.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d209591"],{a958:function(n,e,t){"use strict";t.r(e);var a=function(){var n=this,e=n.$createElement;n._self._c;return n._m(0)},l=[function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",{staticClass:"collaboration-wrapper"},[t("h1",[n._v("This is an collaboration page")])])}],r=t("2877"),c={},s=Object(r["a"])(c,a,l,!1,null,null,null);e["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d209591.b6372e68.js.map
